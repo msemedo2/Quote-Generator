4562285a8e719d5e5f4ec4a14c3e7ddb90a9eb86
unknown <miguel.semedo.2@outlook.com>
2022-09-27 09:14:56 +0100

    Implment Loader

--- scripts.js
+++ scripts.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuotBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // Get Quotes From API
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+	loader.hidden = true;
+	quoteContainer.hidden = false;
+}
+
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://zenquotes.io/api/quotes/';
 	const proxyURL = 'https://cors-anywhere.herokuapp.com/';
 
@@ -18,6 +32,7 @@ async function getQuotes() {
 		const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 		authorText.innerText = quote.a;
 		quoteText.innerText = quote.q;
+		complete();
 	} catch (err) {
 		console.log(err);
 	}
