2d2ecd0af922e86d2d20d342526c7ec1739b4c53
unknown <miguel.semedo.2@outlook.com>
2022-09-27 09:14:24 +0100

    Implement Button Functionality

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-// Get Quotes From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuotBtn = document.getElementById('new-quote');
 
+// Get Quotes From API
 let apiQuotes = [];
 
 async function getQuotes() {
@@ -11,11 +16,22 @@ async function getQuotes() {
 		let apiQuotes = await response.json();
 
 		const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-		console.log(quote);
+		authorText.innerText = quote.a;
+		quoteText.innerText = quote.q;
 	} catch (err) {
 		console.log(err);
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuotBtn.addEventListener('click', getQuotes);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
